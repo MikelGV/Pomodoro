6a9ee398eeca17fad9a43f36132f8b5d8d4b4033
MikelGV <mikelgaldosv@gmail.com>
2021-02-03 05:01:03 +0100

    color removed

--- src/main.py
+++ src/main.py
@@ -4,14 +4,6 @@ import time
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-class Color(QWidget):
-    def __init__(self, color, *args, **kwargs):
-        super(Color, self).__init__(*args, **kwargs)
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
